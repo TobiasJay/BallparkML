b709fb3d2449aeae44505adf4b56c279ecee734b
TobiasJay <1tobyjay@gmail.com>
2023-12-02 20:31:04 -0800

    Solid progress: df indexing and BA cum average over season and 5 game average complete

--- Main.py
+++ Main.py
@@ -1,8 +1,79 @@
 import pandas as pd
+import numpy as np
+import os
+
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 
+'''
+class Data:
+
+    def __init__(self):
+        """
+        Data class.
+
+        Attributes
+        --------------------
+            X -- numpy array of shape (n,d), features
+            y -- numpy array of shape (n,), targets
+        """
+
+        # n = number of examples, d = dimensionality
+        self.X = None
+        self.y = None
+
+        self.Xnames = None
+        self.yname = None
+
+    def load(self, filename, header=0, predict_col=-1):
+        """Load csv file into X array of features and y array of labels."""
+
+        # determine filename
+        dir = os.path.dirname(__file__)
+        f = os.path.join(dir, '..', 'data', filename)
+
+        # load data
+        with open(f, 'r') as fid:
+            data = np.loadtxt(fid, delimiter=",", skiprows=header)
+
+        # separate features and labels
+        if predict_col is None:
+            self.X = data[:,:]
+            self.y = None
+        else:
+            if data.ndim > 1:
+                self.X = np.delete(data, predict_col, axis=1)
+                self.y = data[:,predict_col]
+            else:
+                self.X = None
+                self.y = data[:]
+
+        # load feature and label names
+        if header != 0:
+            with open(f, 'r') as fid:
+                header = fid.readline().rstrip().split(",")
+
+            if predict_col is None:
+                self.Xnames = header[:]
+                self.yname = None
+            else:
+                if len(header) > 1:
+                    self.Xnames = np.delete(header, predict_col)
+                    self.yname = header[predict_col]
+                else:
+                    self.Xnames = None
+                    self.yname = header[0]
+        else:
+            self.Xnames = None
+            self.yname = None
+
+def load_data(filename, header=0, predict_col=-1):
+    """Load csv file into Data class."""
+    data = Data()
+    data.load(filename, header=header, predict_col=predict_col)
+    return data
+'''
 
 def main():
     # Read the CSV file into a pandas DataFrame
@@ -10,7 +81,8 @@ def main():
     b_df = pd.read_csv('data/batterstats.csv')
 
     # Extract the required columns
-    pitcher_columns = ['Date', 'Player-additional', 'IP', 'R', 'Team', 'Opp', 'Result'] # add more features later
+    # Opp_R = Opponent Runs while pitcher was in the game
+    pitcher_columns = ['Date', 'Player-additional', 'IP', 'Opp_R', 'ER', 'Team', 'Opp', 'Result'] # add more features later
     selected_pdata = p_df[pitcher_columns]
     batter_columns = ['Date', 'BA', 'Team', 'Opp'] # add more features later (HR, RBI, etc.)
     selected_bdata = b_df[batter_columns]
@@ -33,26 +105,57 @@ def main():
     # Drop unnecessary columns
     df = df.drop(['Result', 'Outcome', 'Scores'], axis=1)
 
+    '''
     # Line 4016 in dataset marks the start of september, the last month of the season
     training_games = df.head(4016)
     test_games = df.tail(len(df) - 4016)
+    # Then we need to split into features and target (# of runs scored)
+    X = df.drop(['Win', 'Score', 'Opp_R', 'Opp_Score','Player-additional'], axis=1)
+    y = df['Score']
 
-    # Create a dictionary with tuples (Team, Date) as keys and corresponding rows as values for train and test data
-    TrainDict = {}
-    for index, row in training_games.iterrows():
-        key = (row['Team'], row['Date'])
-        TrainDict[key] = row
 
-    TestDict = {}
-    for index, row in test_games.iterrows():
-        key = (row['Team'], row['Date'])
-        TestDict[key] = row
+    # Split into training and test data
+    
+    X_train = X.head(4016)
+    X_test = X.tail(len(X) - 4016)  
+    y_train = y.head(4016)
+    y_test = y.tail(len(y) - 4016)
+    print(X_train)
+    '''
+    # X_train contains Date, BA, Team, Opp, IP, ER
+    
+    # Approach 1. Avg all season stats for each team
+    # 1.1 Create dataset containing average stats to this point, with W - L as the target
+    # 1.2 Repeat for each day of the season to create the rows of the dataset up to 80% of the season
+    # 1.3 Train model on that dataset
+    # 1.4 Test model on the remaining 20% of the season, following same averaging procedure.
+    # 1.5 Improve model by using average of last five games or test on other seasons for more confidence.
 
-    # 2. Train model on training data
+    # 1.1 Create dataset containing average stats to this point, with W - L as the target
+    # Use Team BA, Opp Pitcher ERA (use team ERA if this is pitcher's first game) and skip first game of season
 
-    # prints SEA opponent's stats for the date 2023-10-01
-    print(TestDict.get((TestDict.get(('SEA', '2023-10-01'))['Opp'], '2023-10-01')))
-    # Example: Accessing data for ('SEA', '2023-03-30')
+
+
+
+    # Approach 2. Modify Approach 1 to evaluate only the last 5 games for each team
+
+
+    # sets index to Date and Team so we can quickly reference stats for a given team on a given date
+    df.set_index(['Date', 'Team'], inplace=True)
+
+    # adding column for average of BA to date
+    df['BA_AvgToDate'] = df.groupby('Team')['BA'].cumsum() / (df.groupby('Team').cumcount() + 1)
+    df['PitcherERA_AvgToDate'] = df.groupby(['Team', 'Player-additional'])['IP'].cumsum() / (df.groupby(['Team', 'Player-additional']).cumcount() + 1)
+
+    # adding column for average of BA over last 5 games
+    column_to_average = 'BA'
+    window_size = 5
+
+    # Calculate the rolling mean over the last five games
+    df['BA_AvgLast5'] = df.groupby('Team')[column_to_average].transform(lambda x: x.rolling(window=window_size, min_periods=1).mean())
+
+    print(df.loc['2023-03-30', ...]) 
+
 
 
 if __name__ == '__main__':
